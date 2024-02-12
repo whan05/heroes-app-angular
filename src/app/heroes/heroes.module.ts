cad742f9314eced5428806bf6ec724d7a4d86cb0
Whanderley Fonseca <wfonsecap-m@ucenfotec.ac.cr>
2024-02-11 23:18:31 -0600

    add features as create, edit and delete heroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -11,6 +11,8 @@ import { MaterialModule } from '../material/material.module';
 import { CardComponent } from './components/card/card.component';
 import { HeroImagePipe } from './pipes/hero-image.pipe';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+
 
 
 @NgModule({
@@ -23,7 +25,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     CardComponent,
 
     // Pipes
-    HeroImagePipe
+    HeroImagePipe,
+     ConfirmDialogComponent,
   ],
   imports: [
     CommonModule,
